e37264f21b41cc7fd7aa26654045b35028c587f3
Kovács Lajos <kovacs.lajos1218@gmail.com>
2017-05-30 02:53:56 +0200

    Fixed test issue.

--- tests/common/Loader.test.js
+++ tests/common/Loader.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Loader from '../../src/components/common/Loader';
-import {expect} from 'jest';
+
 
 test('Loader should render', () => {
   const component = renderer.create(
